9f922da172e57bc24e47094338e570c3240b7654
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-01 12:38:33 +0700

    Create tinhgiatritrungbinh5so.py

---
+++ tinhgiatritrungbinh5so.py
@@ -0,0 +1,7 @@
+a =float(int(input("Nhập giá trị của a:")))
+b =float(int(input("Nhập giá trị của b:")))
+c =float(int(input("Nhập giá trị của c:")))
+d =float(int(input("Nhập giá trị của d:")))
+e =float(int(input("Nhập giá trị của e:")))
+giatritrungbinh= (a+b+c+d+e)/5
+print ("Giá trị trung bình là:",giatritrungbinh)
