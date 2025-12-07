ff097c3af713eda20f300c60ff97f101ab8919e0
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-07 11:28:21 +0700

    Create timMAXcua3so.py

---
+++ timMAXcua3so.py
@@ -0,0 +1,9 @@
+a = float(input("Nhập số a:"))
+b = float(input("Nhập số b:"))
+c = float(input("Nhập sô c:"))
+if a > b >= c:
+  print (" Số a là số lớn nhất")
+if b > a >= c:
+  print ("Số b là số lớn nhất")
+if c > b >= a:
+  print ("Số c là số lớn nhất")
