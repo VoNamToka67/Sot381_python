42ebc0f580adcfdc1976db291d459a966d93131d
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-15 17:30:20 +0700

    Create timsoHoanHao.py

---
+++ timsoHoanHao.py
@@ -0,0 +1,9 @@
+n = int(input("Nhập số n: "))
+tong = 0
+for i in range(1, n):
+    if n % i == 0:
+        tong +=  i
+if tong == n:
+    print(n, "là số hoàn hảo")
+else:
+    print(n, "không phải là số hoàn hảo")
