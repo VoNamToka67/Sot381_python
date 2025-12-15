33fc9adccc6f1f60f533ef3a9a7bad1bbcc95470
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-15 14:46:34 +0700

    Create TH_Bai3_W1.py

---
+++ TH_Bai3_W1.py
@@ -0,0 +1,7 @@
+while True:
+    user = input('Vui lòng nhập chữ "Y" hoặc "y": ')    
+    if user == 'Y' or user == 'y':
+        print("Login success!")
+        break  
+    else:        
+        print("Nhập sai rồi, thử lại nhé.")
