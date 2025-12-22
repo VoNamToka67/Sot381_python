683965e4e5aaaf6e44e424439b2767a9e5f48ae7
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-22 16:16:36 +0700

    Update TH2212_Bai6.py

--- TH2212_Bai6.py
+++ TH2212_Bai6.py
@@ -1,7 +1,7 @@
 n = int(input("Nhap so luong bai hat:"))
 danhsachbaihat= []
 for i in range(n):
-    baihat = (input(f"Bai hat thu {i+1} la:"))
+    baihat = input(f"Bai hat thu {i+1} la:")
     danhsachbaihat.append(baihat)
-print(dsbaihat)
+print(danhsachbaihat)
     
