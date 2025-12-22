579b47547d8bb255beadc1f83d506197427d028b
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-22 14:32:05 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -2,15 +2,15 @@ while True:
     m = float(input("Nhap chieu dai:"))
     if m >= 0:
         break
+    else:
+        print("So ko hop le")
 while True:
     n = float(input("Nhap chieu rong:"))
     if n <=100:
         break
+    else:
+        print("so ko hop le")
 P = (m+n)*2
 S = m*n
 print (f"Dien tich hcn la :{S:.2f}")
-print (f"Chu vi hcn la :{P:.2f}")
-    
-    
-    
-    
+print (f"Chu vi hcn la :{P:.2f}")   
