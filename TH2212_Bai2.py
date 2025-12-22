7badd09c14d1417909188d541d7fd91afaa8d045
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-22 14:47:00 +0700

    Create TH2212_Bai2.py

---
+++ TH2212_Bai2.py
@@ -0,0 +1,20 @@
+a = int(input("Nhap so a:"))
+b = int(input("Nhap so b:"))
+c = int(input("Nhap so c:"))
+ds = [a,b,c]
+for i in ds:
+    gtln = a
+    if b > gtln:
+        gtln = b
+    if c > gtln:
+        gtln = c
+for i in ds:
+    gtnn = a
+    if b < gtnn:
+        gtnn = b
+    if c < gtnn:
+        gtnn = c
+print (f"Gia tri lon nhat la :{gtln}")
+print (f"Gia tri nho nhat la :{gtnn}")
+    
+
