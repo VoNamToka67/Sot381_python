2e0a8c191346669053e6f82e1a848bcfc61008d4
Nam67.KHMT <nam.vnh.67khmt@ntu.edu.vn>
2025-12-29 17:21:30 +0700

    Create BaiTapCau1A.py

---
+++ BaiTapCau1A.py
@@ -0,0 +1,11 @@
+n = float(input("Nhap diem trung binh"))
+if n>=8 :
+    print ("Hoc Sinh Gioi")
+if n < 8 and n >= 6.5:
+    print ("Hoc Sinh Kha")
+if n < 6.5 and n >=5:
+    print ("Hoc sinh Trung Binh")
+if n < 5 and n >= 3.5 :
+    print ("Hoc sinh yeu")
+if n < 3.5:
+    print ("Hoc Sinh kem")
